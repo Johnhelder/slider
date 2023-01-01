dd3dd6a3aa7da4df39709201a7e3c7ddafc64527
John Helder <johnhelderjp@gmail.com>
2023-01-01 10:42:10 -0300

    Selecionando os controles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 let totalSlides = document.querySelectorAll('.slider-item').length;
 
 document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
+
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
