edf84e27dcc3e236f94c8a42d9c474431efb7f8c
John Helder <johnhelderjp@gmail.com>
2023-01-02 20:30:21 -0300

    Criando a função goPrev

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ let currentSlide = 0;
 document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
 
 document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev(){
+  currentSlide--;
+  if(currentSlide < 0){
+    currentSlide = totalSlides -1
+  }
+}
