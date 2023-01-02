27615e281b80cbbed94f6d5ca2d2e51d46996caa
John Helder <johnhelderjp@gmail.com>
2023-01-02 20:24:55 -0300

    Criando variavel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let totalSlides = document.querySelectorAll('.slider-item').length;
+let currentSlide = 0;
 
 document.querySelector('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
 
